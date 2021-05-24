e0488d777052e26dc75c7a961cc1f25d7653e472
ParkKwonsoo <redbean096@khu.ac.kr>
2021-05-25 03:35:58 +0900

    feat. pdf views/urls/settings

--- server/lectureScript/lectureScript/urls.py
+++ server/lectureScript/lectureScript/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('user/', include('user.urls')),
     path('records/', include('records.urls')),
+    path('pdf/', include('pdf.urls')),
     path('admin/', admin.site.urls),
 ]
--- server/lectureScript/pdf/views.py
+++ server/lectureScript/pdf/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import PdfSerializers
+
+class PdfView(APIView) :
+    def get(self, request) :
+        return Response('get', status = status.HTTP_200_OK)
+
+    def post(self, request) :
+        return Response('post', status = status.HTTP_200_OK)
+
+
 
 # Create your views here.
