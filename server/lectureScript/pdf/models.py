f614c3e8bd781f86aa2e0beb0ad57eecb2d50d09
ParkKwonsoo <redbean096@khu.ac.kr>
2021-05-25 03:24:33 +0900

    feat. pdf api

--- server/lectureScript/pdf/models.py
+++ server/lectureScript/pdf/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class Pdf(models.Model) :
+    email = models.CharField(max_length = 50)
+    records = models.ForeignKey("records.Recording", on_delete = models.DO_NOTHING)
+
+    def __str__(self) :
+        return self.records
+
 # Create your models here.
