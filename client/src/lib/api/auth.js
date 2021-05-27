a92ba0c0e8dc30b1421dce5e150278b2aa840b2a
ParkKwonsoo <redbean096@khu.ac.kr>
2021-05-27 17:45:54 +0900

    feat. api : auth, pdf

--- client/src/lib/api/auth.js
+++ client/src/lib/api/auth.js
@@ -1,1 +1,14 @@
 import client from './client';
+
+const register = ({email, name, password}) => 
+    client.post('/user/rest-auth/registration/', { email, name, password })
+
+const login = ({email, password}) =>
+    client.post('/user/rest-auth/login/', { email, password })
+
+const logout = () =>
+    client.post('/user/rest-auth/logout/', null);
+
+export default register;
+export default login;
+export default logout;
--- client/src/lib/api/pdf.js
+++ client/src/lib/api/pdf.js
@@ -1,1 +1,10 @@
 import client from './client';
+
+const makePdf = ({ title, professor, createdDate, typeScript, token }) =>
+    client.post('/pdf/pdf/', { title, professor, createdDate, typeScript }, {
+        headers : {
+            'Authorization' : `token ${token}`
+        }
+    })
+
+export default makePdf;
