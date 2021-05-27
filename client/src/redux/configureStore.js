0a7b5dcc1dbf832bf41ec423064392380df1a63c
ParkKwonsoo <redbean096@khu.ac.kr>
2021-05-27 17:46:26 +0900

    fix. module name error

--- client/src/redux/configureStore.js
+++ client/src/redux/configureStore.js
@@ -1,11 +1,11 @@
-import { createStore, applyMiddleWare } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import penderMiddleWare from 'redux-pender';
 import reducer from './modules'
 
 const configureStore = (initialState) => {
     const store = createStore(reducer, initialState, composeWithDevTools(
-        applyMiddleWare(penderMiddleWare())
+        applyMiddleware(penderMiddleWare())
     ));
     
     return store;
