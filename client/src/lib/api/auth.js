cc574d648ae8ece3da00cdae5064fd5b77d7c2e9
ParkKwonsoo <redbean096@khu.ac.kr>
2021-05-27 17:58:17 +0900

    feat. auth and record api

--- client/src/lib/api/auth.js
+++ client/src/lib/api/auth.js
@@ -1,9 +1,9 @@
 import client from './client';
 
-const register = ({email, name, password}) => 
+const register = ({ email, name, password }) => 
     client.post('/user/rest-auth/registration/', { email, name, password })
 
-const login = ({email, password}) =>
+const login = ({ email, password }) =>
     client.post('/user/rest-auth/login/', { email, password })
 
 const logout = () =>
--- client/src/lib/api/record.js
+++ client/src/lib/api/record.js
@@ -1,1 +1,26 @@
 import client from './client';
+
+const makeRecordFile = ({ title, professor, file, token }) =>
+    client.post('/records/recording/', { title, professor, file }, {
+        headers : {
+            'Authorization' : `token ${token}`
+        }
+    })
+
+const getRecordList = ({ token }) =>
+    client.get('/records/recording/', null, {
+        headers : {
+            'Authorization' : `token ${token}`
+        }
+    })
+
+const getRecordById = ({ record_id, token }) =>
+    client.get('/record/recording/' + record_id, null, {
+        headers : {
+            'Authorization' : `token ${token}`
+        }
+    })
+
+export default makeRecordFile;
+export default getRecordList;
+export default getRecordById;
